857cd18eeec7151b2d9feedc3b3c2fee5d7442e9
Peter deHaan <peter@deseloper.com>
2022-08-25 16:32:29 -0700

    FIRST BLOOD

---
+++ .eleventy.js
@@ -0,0 +1,13 @@
+/**
+ * @typedef {import('@11ty/eleventy/src/UserConfig')} EleventyConfig
+ * @typedef {ReturnType<import('@11ty/eleventy/src/defaultConfig')>} EleventyReturnValue
+ * @type {(eleventyConfig: EleventyConfig) => EleventyReturnValue}
+ */
+module.exports = function (eleventyConfig) {
+  return {
+    dir: {
+      input: "src",
+      output: "www",
+    }
+  };
+};
